d7a1f46448049297c6b7573a5cccaa5b9ea2a41c
Rishabh Singh <rishabhdhami090@gmail.com>
2026-03-27 15:39:42 +0530

    add mcp server and instance

---
+++ mcp_server/server.py
@@ -0,0 +1,11 @@
+from mcp_instance import mcp
+
+import tools.flights
+import tools.activities
+import tools.itinerary
+import tools.restaurants
+import tools.hotels
+
+
+if __name__ == "__main__":
+    mcp.run()
